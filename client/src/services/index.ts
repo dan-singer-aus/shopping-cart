121effe67fdb1760b7e4fa01beb46537502706fd
Daniel <90185651+dan-singer-aus@users.noreply.github.com>
2025-06-06 15:12:27 +1000

    added zod

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,50 +1,83 @@
 import axios from "axios";
-import type { Product, ProductInput, CartItem, UpdateSet } from "../types";
+import { productSchema, cartItemSchema, updateSetSchema } from "../types";
+import type { ProductInput, Product } from "../types";
+import { z } from "zod/v4";
 
 const baseUrl = "/api";
 
 const getAllProducts = async () => {
-  const response = await axios.get<Product[]>(`${baseUrl}/products`);
-  return response.data;
+  try {
+    const response = await axios.get(`${baseUrl}/products`);
+    return z.array(productSchema).parse(response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const createNewProduct = async (productInput: ProductInput) => {
-  const response = await axios.post<Product>(
-    `${baseUrl}/products`,
-    productInput
-  );
-  return response.data;
+  try {
+    const response = await axios.post(`${baseUrl}/products`, productInput);
+    return productSchema.parse(response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const editProduct = async (product: Product) => {
-  const response = await axios.put<Product>(
-    `${baseUrl}/products/${product._id}`,
-    product
-  );
-  return response.data;
+  try {
+    const response = await axios.put(
+      `${baseUrl}/products/${product._id}`,
+      product
+    );
+    return productSchema.parse(response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const deleteProduct = async (productId: string) => {
-  await axios.delete(`${baseUrl}/products/${productId}`);
-  return null;
+  try {
+    await axios.delete(`${baseUrl}/products/${productId}`);
+    return null;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const getAllCartProducts = async () => {
-  const response = await axios.get<CartItem[]>(`${baseUrl}/cart`);
-  return response.data;
+  try {
+    const response = await axios.get(`${baseUrl}/cart`);
+    return z.array(cartItemSchema).parse(response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const addToCart = async (productId: string) => {
-  const response = await axios.post<UpdateSet>(`${baseUrl}/add-to-cart`, {
-    productId,
-  });
-  console.log(response);
-  return response.data;
+  try {
+    const response = await axios.post(`${baseUrl}/add-to-cart`, {
+      productId,
+    });
+    return updateSetSchema.parse(response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 const checkout = async () => {
-  await axios.post(`${baseUrl}/checkout`);
-  return null;
+  try {
+    await axios.post(`${baseUrl}/checkout`);
+    return null;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export default {
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,15 +1,32 @@
-export type Product = {
-  _id: string;
-  title: string;
-  quantity: number;
-  price: number;
-};
+import { z } from "zod/v4";
 
-export type ProductInput = Omit<Product, "_id">;
+export const productSchema = z.object({
+  _id: z.string(),
+  title: z.string(),
+  quantity: z.number(),
+  price: z.number(),
+  createdAt: z.iso.datetime().optional(),
+  updatedAt: z.iso.datetime().optional(),
+  __v: z.number().optional(),
+});
 
-export type CartItem = Product & { productId: string };
+export type Product = z.infer<typeof productSchema>;
 
-export type UpdateSet = {
-  product: Product;
-  item: CartItem;
-};
+export const cartItemSchema = productSchema.extend({
+  productId: z.string(),
+});
+
+export type CartItem = z.infer<typeof cartItemSchema>;
+
+export const productInput = productSchema.omit({
+  _id: true,
+});
+
+export type ProductInput = z.infer<typeof productInput>;
+
+export const updateSetSchema = z.object({
+  product: productSchema,
+  item: cartItemSchema,
+});
+
+export type UpdateSet = z.infer<typeof updateSetSchema>;
